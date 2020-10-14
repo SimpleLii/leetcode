fabeb8c66fb015aa5e8ebbd4fca60af6b9ca3499
simplelii <simplelii@163.com>
2020-10-14 13:51:04 +0800

    .gitignore

---
+++ test/src/main/java/simplelii/leetcode/SlideWindow.java
@@ -0,0 +1,17 @@
+package simplelii.leetcode;
+
+/**
+ * 滑动窗口，找到 最大子序和
+ *  eg:
+ *      输入: [-2,1,-3,4,-1,2,1,-5,4]
+ *      输出: 6
+ *      解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * @author liXin
+ * @description
+ * @date 2020/10/14
+ */
+public class SlideWindow {
+    public static void main(String[] args) {
+
+    }
+}
